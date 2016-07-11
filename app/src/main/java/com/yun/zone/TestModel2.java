5d715810376ba184233da2ab9a06e5279c7e557c
叶云 <yeyun@xkeshi.com>
2016-07-11 09:23:02 +0800

    添加分隔符过滤

---
+++ app/src/main/java/com/yun/zone/TestModel2.java
@@ -0,0 +1,33 @@
+package com.yun.zone;
+
+import com.yun.zone.core.ZoneModel;
+
+import java.util.Date;
+
+/**
+ * Created by dell on 2016/7/6.
+ */
+public class TestModel2 extends ZoneModel {
+    public String name;
+    public int age;
+    public Date birthday;
+    public String test1;
+    public String test2;
+    public String test3;
+    public String test4;
+    public String test5;
+    public String test6;
+    public String test7;
+    public String test8;
+    public String test9;
+    public String test10;
+    public String test11;
+    public String test12;
+    public String test13;
+    public String test14;
+    public String test15;
+    public String test16;
+    public String test17;
+
+
+}
